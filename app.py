57129324f5b5cc865776a9b99145e247c65c42dc
0x21106 <ayhan2015007@gmail.com>
2020-01-24 21:29:53 +0400

    converts all .jpg, .jpeg, .png images to 100x100 shape

---
+++ app.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+
+allowed_extensions = (".jpg", ".jpeg", ".png")
+
+for image in os.listdir():
+    image_type = os.path.splitext(f'{os.getcwd()}\{image}');
+    if image_type[1] in allowed_extensions:
+        img = cv2.imread(f'{os.getcwd()}\{image}', 1)
+        resized = cv2.resize(img, (100, 100))
+        cv2.imwrite(f"{image_type[0]}_resized{image_type[1]}", resized)
+
+# resized_image = cv2.resize(img, (500, 500))
+
+
+
+# cv2.imshow("Galaxy", resized_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
